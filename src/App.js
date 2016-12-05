cc0bcf0ef1ab8f805c9d332e8b3988c1aa155910
Daniel Deutsch <deudan1010@gmail.com>
2016-12-05 10:50:45 +0100

    add fetchData, changeLocation function and state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import logo from './logo.svg';//import the logo
 import './App.css';//load the app.css module
 
 class App extends Component {
+  state = {
+    location: 'Vienna, Austria'
+  };
+  fetchData = (evt) => {
+    evt.preventDefault();
+    console.log('fetch data for ', this.state.location);
+  };//create a fetchData function
+  changeLocation = (evt) => {
+    this.setState({
+      location: evt.target.value
+    });
+  };
   render() {
     return (
       <div className="App">
@@ -10,7 +22,19 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <h1>Weather</h1>
+        <div>
+          <h1>Weather</h1>
+          <form onSubmit={this.fetchData}>
+            <label>I want to know the weather for
+              <input
+                placeholder={'City, Country'}
+                type='text'
+                value={this.state.location}
+                onChange={this.changeLocation}
+              />
+            </label>
+          </form>
+        </div>
       </div>
     );
   }
