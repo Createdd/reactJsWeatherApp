1a55c8520c30b4175f315d281a93f5249cde93b6
Daniel Deutsch <deudan1010@gmail.com>
2016-12-05 10:17:59 +0100

    set up css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';//load the react component module
+import logo from './logo.svg';//import the logo
+import './App.css';//load the app.css module
 
 class App extends Component {
   render() {
@@ -10,12 +10,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Weather</h1>
       </div>
     );
   }
 }
 
-export default App;
+export default App;//export the component 'app'
