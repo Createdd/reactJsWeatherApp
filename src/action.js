549893527cf2294fd17f54a44f33d58fc1dc75da
Daniel Deutsch <deudan1010@gmail.com>
2016-12-08 20:05:19 +0100

    update reducer and action js

--- src/action.js
+++ src/action.js
@@ -4,3 +4,15 @@ export function changeLocation(location){
     location: location
   };
 }
+export function setSelectedDate(date) {
+  return {
+    type: 'SET_SELCTED_DATE',
+    date: date
+  };
+}
+export function setSelectedTemp(temp) {
+  return {
+    type: 'SET_SELCTED_TEMP',
+    temp: temp
+  };
+}
