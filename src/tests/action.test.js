45bcf1272e4296e19f40a4e8c92f22098457f03d
Daniel Deutsch <deudan1010@gmail.com>
2016-12-09 17:22:47 +0100

    test selected Date and Temp

--- src/tests/action.test.js
+++ src/tests/action.test.js
@@ -1,5 +1,7 @@
 import {
-  changeLocation
+  changeLocation,
+  setSelectedDate,
+  setSelectedTemp
 } from '../action';
 
 describe('actions', function() {
@@ -7,5 +9,30 @@ describe('actions', function() {
     it('should have a type of "CHANGE_LOCATION"', function(){
       expect(changeLocation().type).toEqual('CHANGE_LOCATION');
     });
+    it('should pass on the location we pass in', function() {
+    var location = 'Vienna, Austria';
+    expect(changeLocation(location).location).toEqual(location);
+    });
+  });
+  describe('setSelectedDate', function() {
+    it('should have a type of SET_SELECTED_DATE', function() {
+            expect(setSelectedDate().type).toEqual('SET_SELECTED_DATE');
+        });
+
+        it('should pass on the date we pass in', function() {
+            var date = '2016-01-01';
+            expect(setSelectedDate(date).date).toEqual(date);
+        });
+  });
+
+  describe('setSelectedTemp', function() {
+    it('should have a type of SET_SELECTED_TEMP', function() {
+            expect(setSelectedTemp().type).toEqual('SET_SELECTED_TEMP');
+        });
+
+        it('should pass on the temp we pass in', function() {
+            var temp = '31';
+            expect(setSelectedTemp(temp).temp).toEqual(temp);
+        });
   });
 });
